84fb75bb9908e2559e9e8829fcc896e95888ed92
BoilingNoob <84647732+BoilingNoob@users.noreply.github.com>
2024-09-08 11:47:08 -0400

    added parameters and re-added the url navigation

--- re-import_subs.ps1
+++ re-import_subs.ps1
@@ -14,13 +14,13 @@ $locate_to_new_url_js
 <<" -ForegroundColor Green
 #>
 
-#set-new_url_location -new_location $url -wait_x_seconds $seconds_after_loading_page -wshell $wshell -locate_to_new_url_js $locate_to_new_url_js
+set-new_url_location -new_location $url -wait_x_seconds $seconds_after_loading_page -wshell $wshell -locate_to_new_url_js $locate_to_new_url_js
 
 function subscribe_to_sub() {
     param(
         $subscribe_normal_js, 
         $wshell,
-        #$wait_after_pasting_grab_JS = 3,
+        $wait_after_pasting_grab_JS = 3,
         $wait_between_check_looks_ms = 500,
         $max_loops = 40,
         $recursion_count = 0
@@ -30,7 +30,7 @@ function subscribe_to_sub() {
     Set-Clipboard $got_clip_board
     $wshell.SendKeys("^a")
     $wshell.SendKeys("^v")
+    Start-Sleep $wait_after_pasting_grab_JS
     $wshell.SendKeys("^~")
-    #Start-Sleep $wait_after_pasting_grab_JS
 }
-subscribe_to_sub -subscribe_normal_js $subscribe_normal_js -wshell $wshell
+subscribe_to_sub -subscribe_normal_js $subscribe_normal_js -wshell $wshell -wait_after_pasting_grab_JS $wait_after_pasting
